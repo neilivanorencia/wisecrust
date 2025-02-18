105d99d036a167ad2a47933b6af4bf8711ce4b1b
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-18 08:48:16 +0800

    Add website logo in navigation bar

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -1,3 +1,16 @@
 export const HeaderSection = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="container mx-auto px-4">
+        <div className="flex h-24 items-center justify-between md:h-28">
+          <div className="flex items-center gap-2">
+            <img src="/favicon.svg" alt="Wisecrust Logo" className="size-20" />
+            <p className="font-sans text-2xl font-semibold tracking-wide text-green-600 uppercase md:text-3xl">
+              Wisecrust
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
