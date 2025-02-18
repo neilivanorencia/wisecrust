6b1e7ab74e57247343e2246055e2e48db51d8971
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-18 09:04:18 +0800

    Add menu button in navigation bar

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -4,10 +4,14 @@ export const HeaderSection = () => {
       <div className="container mx-auto px-4">
         <div className="flex h-24 items-center justify-between md:h-28">
           <div className="flex items-center gap-4">
-            <button className="relative hidden cursor-pointer px-4 py-2 font-semibold tracking-wide uppercase md:inline-flex">
-              <div className="absolute inset-0 outline-2 -outline-offset-2 outline-orange-400"></div>
-              <span className="leading-6 text-orange-400">Explore Now</span>
-            </button>
+            <div className="relative size-10 cursor-pointer">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="h-0.5 w-5 -translate-y-1 bg-gray-700"></div>
+              </div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="h-0.5 w-5 translate-y-1 bg-gray-700"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
