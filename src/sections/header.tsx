3cfaca66c70e12ca010c40d2133454490da1cac0
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-18 09:48:39 +0800

    Structure and style header content

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -1,13 +1,28 @@
 export const HeaderSection = () => {
   return (
-    <header>
+    <header className="sticky top-0 z-50 bg-white/10 backdrop-blur-2xl">
       <div className="container mx-auto px-4">
         <div className="flex h-24 items-center justify-between md:h-28">
+          <div className="flex items-center gap-2">
+            <img src="/favicon.svg" alt="Wisecrust Logo" className="size-20" />
+
+            <p className="font-sans text-2xl font-semibold tracking-wide text-green-600 uppercase md:text-3xl">
+              Wisecrust
+            </p>
+          </div>
+
           <div className="flex items-center gap-4">
+            <button className="relative hidden cursor-pointer px-4 py-2 font-semibold tracking-wide uppercase md:inline-flex">
+              <div className="absolute inset-0 outline-2 -outline-offset-2 outline-orange-400"></div>
+
+              <span className="leading-6 text-orange-400">Explore Now</span>
+            </button>
+
             <div className="relative size-10 cursor-pointer">
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                 <div className="h-0.5 w-5 -translate-y-1 bg-gray-700"></div>
               </div>
+
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                 <div className="h-0.5 w-5 translate-y-1 bg-gray-700"></div>
               </div>
