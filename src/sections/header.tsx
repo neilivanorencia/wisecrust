0d05e23c8c237252f6b187150813b680795e2210
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-18 08:56:36 +0800

    Add explore button in navigation bar

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -3,11 +3,11 @@ export const HeaderSection = () => {
     <header>
       <div className="container mx-auto px-4">
         <div className="flex h-24 items-center justify-between md:h-28">
-          <div className="flex items-center gap-2">
-            <img src="/favicon.svg" alt="Wisecrust Logo" className="size-20" />
-            <p className="font-sans text-2xl font-semibold tracking-wide text-green-600 uppercase md:text-3xl">
-              Wisecrust
-            </p>
+          <div className="flex items-center gap-4">
+            <button className="relative hidden cursor-pointer px-4 py-2 font-semibold tracking-wide uppercase md:inline-flex">
+              <div className="absolute inset-0 outline-2 -outline-offset-2 outline-orange-400"></div>
+              <span className="leading-6 text-orange-400">Explore Now</span>
+            </button>
           </div>
         </div>
       </div>
