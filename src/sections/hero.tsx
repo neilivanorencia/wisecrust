a0b09a2b5f957db27a2753a5757a093a8b438750
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-19 11:44:12 +0800

    Add hero text elements

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,3 +1,19 @@
 export const HeroSection = () => {
-  return <section></section>;
+  return (
+    <section className="overflow-x-clip py-36 md:py-52">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-lg font-bold tracking-wider text-slate-600 uppercase">
+          Haven for Curious Minds
+        </p>
+
+        <h1 className="mx-auto mt-4 max-w-2xl text-center text-5xl font-extrabold text-slate-800 md:text-6xl">
+          Lighting Lamps in the Labyrinth of Life
+        </h1>
+
+        <p className="mx-auto mt-6 max-w-xl text-center font-serif text-2xl text-slate-700 md:text-3xl">
+          Establishing insightful reflection through profound concepts
+        </p>
+      </div>
+    </section>
+  );
 };
