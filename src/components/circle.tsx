313ef517b52e76bb503e398ec7041a89725eb2fd
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-20 08:21:29 +0800

    Refine background and component colors

--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -7,7 +7,7 @@ export const Circle = (props: ComponentPropsWithoutRef<"div">) => {
   return (
     <div
       className={twMerge(
-        "inline-flex size-[250px] items-center justify-center rounded-full bg-green-100 outline-6 -outline-offset-6 outline-green-500/10",
+        "inline-flex size-[250px] items-center justify-center rounded-full bg-green-200 outline-6 -outline-offset-6 outline-green-500/20",
         className,
       )}
     >
