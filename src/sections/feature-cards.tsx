177ef65a2ddfa6267ab79db6b19aa3db2ba1eac2
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-21 10:06:14 +0800

    Add explore card data structure

--- src/sections/feature-cards.tsx
+++ src/sections/feature-cards.tsx
@@ -1,3 +1,41 @@
+const cardData = [
+  {
+    image: "/src/assets/desk-lamp.png",
+    title: "Existentialism",
+    description:
+      "Existentialism posits that existence precedes essence, meaning individuals are born into the world without pre-defined purpose or nature.",
+    color: "emerald",
+  },
+  {
+    image: "/src/assets/whiteboard.png",
+    title: "Nihilism",
+    description:
+      "Nihilism is a philosophical concept that fundamentally questions the existence of objective meaning, value, and truth. ",
+    color: "blue",
+  },
+  {
+    image: "/src/assets/stationery.png",
+    title: "Idealism",
+    description:
+      "Idealism asserts that the nature of this world is fundamentally shaped by, or reducible to, mind, spirit, or consciousness.",
+    color: "teal",
+  },
+  {
+    image: "/src/assets/diploma.png",
+    title: "Solipsism",
+    description:
+      "Solipsism is a philosophical proposition that posits the potential singularity of consciousness.",
+    color: "sky",
+  },
+  {
+    image: "/src/assets/locker.png",
+    title: "Memento Mori",
+    description:
+      "Memento mori, Latin for 'remember you must die,' serves as a potent reminder of the finite nature of human existence.",
+    color: "cyan",
+  },
+];
+
 export const FeatureCards = () => {
   return (
     <section className="-mt-24 overflow-x-clip py-4 md:py-16">
