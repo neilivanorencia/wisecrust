189f0b05b025fdf8e62c6a17655c47b2e373949b
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-22 06:09:11 +0800

    Create static content list for first explore grid component

--- src/sections/feature-grid.tsx
+++ src/sections/feature-grid.tsx
@@ -1,3 +1,9 @@
+const listItems = [
+  "Discover articulate examinations of key philosophical domains.",
+  "Benefit from comprehensive explorations of diverse philosophical concepts.",
+  "Unlock a deeper appreciation for the foundations of thought.",
+];
+
 export const FeatureGrid = () => {
   return (
     <section className="overflow-x-clip py-8">
