a96bcd46fcd5f24213d15a73ed3af71d275c3748
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-21 11:01:20 +0800

    Add card styling layers and color variants

--- src/sections/feature-cards.tsx
+++ src/sections/feature-cards.tsx
@@ -1,3 +1,6 @@
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { twMerge } from "tailwind-merge";
+
 const cardData = [
   {
     image: "/src/assets/desk-lamp.png",
@@ -46,18 +49,53 @@ export const FeatureCards = () => {
 
         <div className="mt-32 flex px-4 max-md:justify-center">
           <div className="flex flex-none gap-8 max-md:[&>*]:hidden max-md:[&>*:first-child]:block">
-            {cardData.map(({ image, title, description }) => (
-              <div key={title} className="max-w-xs p-8 md:max-w-md md:p-10">
-                <img src={image} alt={title} className="size-50" />
+            {cardData.map(({ image, title, description, color }) => (
+              <div
+                key={title}
+                className="group relative z-0 max-w-xs p-8 md:max-w-md md:p-10"
+              >
+                <div
+                  className={twMerge(
+                    "absolute top-1.5 right-1.5 -z-10 size-16 rounded-xl",
+                    color === "emerald" && "bg-emerald-500",
+                    color === "blue" && "bg-blue-500",
+                    color === "teal" && "bg-teal-500",
+                    color === "sky" && "bg-sky-500",
+                    color === "cyan" && "bg-cyan-500",
+                  )}
+                />
+
+                <div className="absolute inset-0 -z-10 rounded-2xl bg-green-300 [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]" />
+
+                <div className="-mt-28 flex justify-center">
+                  <div className="relative inline-flex">
+                    <img src={image} alt={title} className="size-50" />
+                  </div>
+                </div>
+
                 <h3 className="mt-12 font-sans text-3xl font-bold tracking-wide text-slate-800 uppercase">
                   {title}
                 </h3>
+
                 <p className="mt-4 font-serif text-2xl text-slate-800">
                   {description}
                 </p>
-                <button className="mt-12 font-sans text-sm font-bold tracking-wide uppercase">
-                  Learn More
-                </button>
+
+                <div className="mt-12 flex justify-between">
+                  <button
+                    className={twMerge(
+                      "cursor-pointer font-sans text-sm font-bold tracking-wide uppercase",
+                      color === "emerald" && "text-emerald-500",
+                      color === "blue" && "text-blue-500",
+                      color === "teal" && "text-teal-500",
+                      color === "sky" && "text-sky-500",
+                      color === "cyan" && "text-cyan-500",
+                    )}
+                  >
+                    Learn More
+                  </button>
+                  <HiOutlineArrowNarrowRight className="size-8 text-slate-800" />
+                </div>
               </div>
             ))}
           </div>
