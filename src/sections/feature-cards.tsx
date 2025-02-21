680149c02795809d9c1dea0ea658a618ba1a3130
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-21 09:31:24 +0800

    Create base component of explore section

--- src/sections/feature-cards.tsx
+++ src/sections/feature-cards.tsx
@@ -1,5 +1,11 @@
 export const FeatureCards = () => {
   return (
-    <section></section>
+    <section className="-mt-24 overflow-x-clip py-4 md:py-16">
+      <div className="container mx-auto px-4">
+        <h2 className="mx-auto max-w-4xl text-center font-sans text-4xl font-extrabold text-slate-800 md:text-5xl">
+          Explore the Frontiers of Thought with Wisecrust
+        </h2>
+      </div>
+    </section>
   );
 };
