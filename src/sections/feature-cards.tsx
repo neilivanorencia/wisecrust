7b1ccbcd525138266815faa2ffa757753ebe337a
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-23 10:42:17 +0800

    Refine button interface for visual coherence

--- src/sections/feature-cards.tsx
+++ src/sections/feature-cards.tsx
@@ -101,15 +101,23 @@ export const FeatureCards = () => {
                 <div className="mt-12 flex justify-between">
                   <button
                     className={twMerge(
-                      "cursor-pointer font-sans text-sm font-bold tracking-wide uppercase",
-                      color === "emerald" && "text-emerald-500",
-                      color === "blue" && "text-blue-500",
-                      color === "teal" && "text-teal-500",
-                      color === "sky" && "text-sky-500",
-                      color === "cyan" && "text-cyan-500",
+                      "group relative flex h-[38px] cursor-pointer items-center font-sans font-bold tracking-wide uppercase transition-all duration-300",
+                      color === "emerald" && "text-emerald-500 hover:text-emerald-400",
+                      color === "blue" && "text-blue-500 hover:text-blue-400",
+                      color === "teal" && "text-teal-500 hover:text-teal-400",
+                      color === "sky" && "text-sky-500 hover:text-sky-400",
+                      color === "cyan" && "text-cyan-500 hover:text-cyan-400",
                     )}
                   >
                     Learn More
+                    <span className={twMerge(
+                      "absolute -bottom-0 left-0 h-[2px] w-0 transition-all duration-300 group-hover:w-full",
+                      color === "emerald" && "bg-emerald-500",
+                      color === "blue" && "bg-blue-500",
+                      color === "teal" && "bg-teal-500",
+                      color === "sky" && "bg-sky-500",
+                      color === "cyan" && "bg-cyan-500",
+                    )}></span>
                   </button>
 
                   <HiOutlineArrowNarrowRight className="size-8 -translate-x-2 text-slate-800 transition duration-300 group-hover:translate-x-0 group-hover:text-slate-600" />
