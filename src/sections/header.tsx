07c6fc11a40384a1ea3fcb088f928558a72a84fe
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-02-27 07:05:35 +0800

    Display latest post on articles index

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -9,8 +9,8 @@ const navigationLinks = [
     href: "/",
   },
   {
-    title: "Blogs",
-    href: "/blogs",
+    title: "Articles",
+    href: "/articles",
   },
   {
     title: "Courses",
@@ -117,10 +117,10 @@ export const HeaderSection = () => {
           >
             <div className="absolute inset-2 mt-24 md:mt-28">
               <div className="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2">
-                <Hexagon size={700} />
+                <Hexagon size={1000} />
               </div>
               <div className="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2">
-                <Hexagon size={1100} />
+                <Hexagon size={1400} />
               </div>
               <div className="flex h-full items-center justify-center">
                 <nav className="flex flex-col items-center gap-12 md:gap-16">
